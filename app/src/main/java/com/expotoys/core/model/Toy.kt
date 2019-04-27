7681cc1a06c8ee70128ad5e9543740c6843262a2
Alex Campayo <alex.campayo@gmail.com>
2019-04-27 18:11:10 +0200

    Add Toy model

---
+++ app/src/main/java/com/expotoys/core/model/Toy.kt
@@ -0,0 +1,13 @@
+package com.expotoys.core.model
+
+data class Toy (
+    val name: String,
+    val technique: String,
+    val size: String,
+    val description: String,
+    val biography: String,
+    val price: String,
+    val instagramUrl: String,
+    val paypalAccount: String,
+    val photos: List<String>
+)
